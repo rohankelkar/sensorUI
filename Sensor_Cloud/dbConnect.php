46291b3e21c96f6483f7e7556c5d90e9128ca499
Rohan Kelkar <rohan.kelkar@sjsu.edu>
2018-05-07 13:25:27 -0700

    Required Database Changes
    Required Database Changes

--- Sensor_Cloud/dbConnect.php
+++ Sensor_Cloud/dbConnect.php
@@ -1,8 +1,8 @@
 <?php
-   define('DB_SERVER', "server address");
-   define('DB_USERNAME', "username");
-   define('DB_PASSWORD', "password");
-   define('DB_DATABASE', "databasename");
+   define('DB_SERVER', "cmpe272.c7heilvdo1fe.us-east-1.rds.amazonaws.com");
+   define('DB_USERNAME', "root");
+   define('DB_PASSWORD', "bsrihari09");
+   define('DB_DATABASE', "SENSOR_CLOUD");
   
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
